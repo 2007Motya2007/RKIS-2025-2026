03bbc684b1ce8a8af85e1bedd813345baf09dd42
2007Motya2007 <2007ninai2007@gmail.com>
2026-03-24 22:30:17 +0300

    lesson 3: add profile command

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -12,8 +12,19 @@
 
 			Console.Write("Введите ваш год рождения: ");
 			int year = int.Parse(Console.ReadLine());
+			int age = DateTime.Now.Year - year;
+			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {age}");
 
-			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {DateTime.Now.Year - year}");
+			while (true)
+			{
+				Console.Write("Введите команду: ");
+				string command = Console.ReadLine();
+
+				if (command == "profile")
+				{
+					Console.WriteLine(firstName + " " + lastName + ", - " + age);
+				}
+			}
 		}
 	}
 }
