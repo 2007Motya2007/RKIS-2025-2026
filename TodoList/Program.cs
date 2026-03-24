d9c77ee9f93d32486b924d53a07eaac2001222b3
2007Motya2007 <2007ninai2007@gmail.com>
2026-03-24 22:35:50 +0300

    lesson 4: clean up vars

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,22 +2,17 @@
 {
 	class Program
 	{
+		private static string firstName, lastName;
+		private static int age;
+
+		private static string[] todos = new string[2];
+		private static int index;
+
 		public static void Main()
 		{
 			Console.WriteLine("Работу выполнили Сироткин и Галои 3834");
-			Console.Write("Введите ваше имя: ");
-			string firstName = Console.ReadLine();
-			Console.Write("Введите вашу фамилию: ");
-			string lastName = Console.ReadLine();
-
-			Console.Write("Введите ваш год рождения: ");
-			int year = int.Parse(Console.ReadLine());
-			int age = DateTime.Now.Year - year;
-			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {age}");
-
-			string[] todos = new string[2];
-			int index = 0;
-
+			AddUser();
+			
 			while (true)
 			{
 				Console.Write("Введите команду: ");
@@ -76,6 +71,20 @@
 					Console.WriteLine("Неизвестная команда.");
 				}
 			}
+		}
+
+		private static void AddUser()
+		{
+			Console.Write("Введите ваше имя: ");
+			firstName = Console.ReadLine();
+			Console.Write("Введите вашу фамилию: ");
+			lastName = Console.ReadLine();
+
+			Console.Write("Введите ваш год рождения: ");
+			var year = int.Parse(Console.ReadLine());
+			age = DateTime.Now.Year - year;
+
+			Console.WriteLine("Добавлен пользователь " + firstName + " " + lastName + ", возраст - " + age);
 		}
 	}
 }
