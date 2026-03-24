e618b2e255ff32ce285832baaf42c2c15bbe50d6
2007Motya2007 <2007ninai2007@gmail.com>
2026-03-24 22:31:57 +0300

    lesson 3: add view command

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -15,6 +15,9 @@
 			int age = DateTime.Now.Year - year;
 			Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст - {age}");
 
+			string[] todos = new string[2] { "test", "test" };
+			int index = 0;
+
 			while (true)
 			{
 				Console.Write("Введите команду: ");
@@ -25,16 +28,29 @@
 					Console.WriteLine("Команды:");
 					Console.WriteLine("help — выводит список всех доступных команд с кратким описанием");
 					Console.WriteLine("profile — выводит данные пользователя");
+					Console.WriteLine("view — выводит все задачи");
 					Console.WriteLine("exit — выход из программы");
 				}
 				else if (command == "profile")
 				{
 					Console.WriteLine(firstName + " " + lastName + ", - " + age);
 				}
+
 				else if (command == "exit")
 				{
 					Console.WriteLine("Выход из программы.");
 					break;
+				}
+				else if (command == "view")
+				{
+					Console.WriteLine("Задачи:");
+					foreach (string todo in todos)
+					{
+						if (!string.IsNullOrEmpty(todo))
+						{
+							Console.WriteLine(todo);
+						}
+					}
 				}
 				else
 				{
